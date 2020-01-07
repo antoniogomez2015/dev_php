26fec96567dceae910a1ae583ed92b884708fd80
tonyGomez <monoproduccion.com>
2020-01-07 17:26:39 +0100

    Se ha inicializado el repositorio de PHP, 07 de Enero del 2020

---
+++ Estructura_y_Lenguaje/01_variables.php
@@ -0,0 +1,27 @@
+<?php
+
+
+	/*
+
+		Variables:
+		__________
+
+
+		_las variables son pequeños contenedores a través, de los cuáles, podremos guardar información en la memoria del sistema ó equipo y a la cuál, podremos acceder por medio del lenguaje de programación, en éste caso php
+
+
+
+		Ejemplo:
+		________
+
+
+	*/
+
+	
+	var nombre = "tony";
+
+	echo nombre;
+
+
+
+?>
